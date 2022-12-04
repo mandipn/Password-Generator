4295612443e62f51c1432845fd5f6dac2f8a435a
Mandip Nijor <mandip.nijor@gmail.com>
2022-12-04 13:00:14 +0000

    adding comments to the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-function generatePassword() {
-    var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
-var possibleCharacters = [];
+// creating the generate password function and variables for each type of character
 
-// get input and validate
+function generatePassword() {
+    var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+    var possibleCharacters = [];
+
+// getting the input and validating against the number of characters required
+
 numberOfCharacters = prompt("How many characters would you like in your password? Choose between 10-64 characters.");
 if (numberOfCharacters < 10 || numberOfCharacters > 64) {
   return "Please choose at least 10 characters but no more than 64";
@@ -17,6 +20,8 @@ else {
   
 }
 
+// asking whether the user would like lower case characters then prompting accordingly
+
 hasLowercase = confirm("Would you like lowercase characters in your password?");
 if (hasLowercase) {
   var turnToLowercase = alert("Your password will have lowercase characters.");
@@ -24,6 +29,8 @@ if (hasLowercase) {
 else {
   alert("Your password will NOT have lowercase characters.");
 }
+
+// asking whether the user would like upper case characters then prompting accordingly
 
 hasUppercase = confirm("Would you like uppercase characters in your password?");
 if (hasUppercase) {
@@ -33,6 +40,8 @@ else {
   alert("Your password will NOT have uppercase characters.");
 }
 
+// asking whether the user would like numbers then prompting accordingly
+
 hasNumbers = confirm("Would you like to use numbers in your password?");
 if (hasNumbers) {
   alert("Your password will have numbers.");
@@ -40,6 +49,8 @@ if (hasNumbers) {
 else {
   alert("Your password will NOT have numbers.");
 }
+
+// asking whether the user would like special characters then prompting accordingly
 
 hasSpecial = confirm("Would you like special characters in your password?");
 if (hasSpecial) {
@@ -49,11 +60,14 @@ else {
   alert("Your password will NOT have special characters.");
 }
 
+// if user cancels all of the above selection types then prompting them to select at least one type
+
 if (hasLowercase === false && hasUppercase === false && hasNumbers === false && hasSpecial === false) {
   return "Please select at least one character type.";
 };
 
-// group selected characters
+// grouping (concating) together the selected characters
+
 if (hasLowercase) {
   possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
 }
@@ -67,7 +81,8 @@ if (hasSpecial) {
   possibleCharacters = possibleCharacters.concat(specialCharacters);
 }
 
-// pick random cards out of new pool for length of password
+// picking random cards for the length of password
+
 let finalPassword = ""
 for (let i = 0; i < numberOfCharacters; i++) {
   let rng =[Math.floor(Math.random() * possibleCharacters.length)];
@@ -77,15 +92,18 @@ for (let i = 0; i < numberOfCharacters; i++) {
 return finalPassword;
 };
 
-// Get references to the #generate element
+// Getting the references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Writing the password to the #password input
+
 function writePassword() {
 var password = generatePassword();
 var passwordText = document.querySelector("#password");
 passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Adding the event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
